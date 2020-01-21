468a1c066e12734c6c5a90451a166b2b1c20f7fd
jsbourdon <bourdon.js@gmail.com>
2020-01-21 09:50:03 -0500

    WIP commit O_o

---
+++ Memory/ThreadHeapSmartPointer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace salvation
+{
+    namespace memory
+    {
+        template<typename T>
+        struct ThreadHeapSmartPointer
+        {
+            ThreadHeapSmartPointer(void *pPtr) : m_pPtr(static_cast<T*>(pPtr)) {}
+            ~ThreadHeapSmartPointer() { ThreadHeapAllocator::Release(m_pPtr); }
+
+            inline operator T*() { return m_pPtr; }
+            inline T& operator[](size_t index) { return m_pPtr[index]; }
+            inline T* operator+(size_t offset) { return m_pPtr + offset; }
+            inline T* operator->() { return m_pPtr; }
+            inline T operator*() { return *m_pPtr; }
+
+        private:
+            T *m_pPtr;
+        };
+    }
+}
