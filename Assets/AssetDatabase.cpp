903dac9955d723ce775de25a38030095293a0b9a
jsbourdon <jean.sebastien.bourdon@tripleboris.com>
2020-01-27 15:24:17 -0500

    * Database generation completed * Mesh packaging completed * Texture compression and packaging completed

---
+++ Assets/AssetDatabase.cpp
@@ -0,0 +1,26 @@
+#include <pch.h>
+#include "AssetDatabase.h"
+
+using namespace salvation::asset;
+
+static constexpr size_t s_ComponentTypeByteSizes[] =
+{
+    4,  // Scalar_Float
+    1,  // Scalar_Byte,
+    2,  // Scalar_Short
+    4,  // Scalar_Int
+    8,  // Vec2
+    12, // Vec3
+    16, // Vec4
+    16, // Matrix2x2
+    36, // Matrix3x3
+    64, // Matrix4x4
+};
+
+static_assert(ARRAY_SIZE(s_ComponentTypeByteSizes) == static_cast<size_t>(ComponentType::Count));
+
+uint32_t AssetDatabase::ComponentTypeByteSize(ComponentType type)
+{
+    SALVATION_ASSERT(static_cast<int32_t>(type) >= 0 && static_cast<int32_t>(type) < static_cast<int32_t>(ComponentType::Count), "");
+    return static_cast<uint32_t>(s_ComponentTypeByteSizes[static_cast<size_t>(type)]);
+}
--- Assets/AssetDatabase.h
+++ Assets/AssetDatabase.h
@@ -22,9 +22,39 @@ namespace salvation
             Count
         };
 
+        enum class ComponentType
+        {
+            Unknown = -1,
+            Scalar_Float,
+            Scalar_Byte,
+            Scalar_Short,
+            Scalar_Int,
+            Vec2,
+            Vec3,
+            Vec4,
+            Matrix2x2,
+            Matrix3x3,
+            Matrix4x4,
+            Count
+        };
+
+        enum class AttributeSemantic
+        {
+            Unknown = -1,
+            Position,
+            Normal,
+            Tangent,
+            TexCoord0,
+            TexCoord1,
+            Color,
+            Count
+        };
+
         class AssetDatabase
         {
+        public:
 
+            static uint32_t ComponentTypeByteSize(ComponentType type);
         };
     }
 }
